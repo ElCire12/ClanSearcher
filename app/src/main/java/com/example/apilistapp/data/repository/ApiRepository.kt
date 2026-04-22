27c02c50dfb63f9a22b96156faa86b0e8e33d9c0
sjo <prueba@gmail.com>
2026-04-22 09:54:50 +0200

    cambios

--- app/src/main/java/com/example/apilistapp/data/repository/ApiRepository.kt
+++ app/src/main/java/com/example/apilistapp/data/repository/ApiRepository.kt
@@ -1,10 +1,45 @@
 package com.example.apilistapp.data.repository
 
+import com.example.apilistapp.data.mapper.toDomain
 import com.example.apilistapp.data.remote.ApiInterface
+import com.example.apilistapp.domain.ClanDomain
 
 class ApiRepository {
     val apiInterface = ApiInterface.create()
-    suspend fun getClans() = apiInterface.getClansList(locationId = 32000218)
-    suspend fun searchClan(name: String?) = apiInterface.getClansList(name = name, limit = 10)
-    suspend fun getClanInfo(tag: String) = apiInterface.getClanInfo(clanTag = tag)
+    suspend fun getClans(): List<ClanDomain>? {
+        val clansList: List<ClanDomain>?
+        val response = apiInterface.getClansList(locationId = 32000218)
+
+        if (response.isSuccessful) {
+            clansList = response.body()!!.clans.map { it.toDomain() }
+        } else clansList = null
+
+        return clansList
+    }
+
+    suspend fun searchClan(name: String?): List<ClanDomain>? {
+        val clansList: List<ClanDomain>?
+        val response = apiInterface.getClansList(name = name, limit = 10)
+
+        if (response.isSuccessful) {
+            clansList = response.body()!!.clans.map { it.toDomain() }
+        } else clansList = null
+
+        return clansList
+    }
+
+    suspend fun getClanInfo(tag: String): ClanDomain? {
+        val clan: ClanDomain?
+        val response = apiInterface.getClanInfo(clanTag = tag)
+
+        if (response.isSuccessful){
+            clan = response.body()!!.toDomain()
+        }
+        else clan = null
+
+        return clan
+    }
+
+    //fun errorMessage(response: Response)
+
 }
