b70940ed7294ab6182ba7f80516ef9fc17ffc80c
Èric Moral <eric.moral.7e7@itb.cat>
2026-04-24 00:01:58 +0200

    Dialog implementado

--- app/src/main/java/com/example/apilistapp/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/apilistapp/ui/screens/settings/SettingsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -17,12 +18,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.apilistapp.ui.components.DropDownComponent
 
 @Composable
 fun SettingsScreen(settings: SettingsViewModel) {
-    // Instancia del ViewModel siguiendo el estilo de tu clase
 
     val isDarkMode by settings.isDarkMode.collectAsStateWithLifecycle()
 
@@ -30,6 +29,8 @@ fun SettingsScreen(settings: SettingsViewModel) {
     var gridDropDownExpanded by remember { mutableStateOf(false) }
     val gridOptions =
         listOf("List", "Grid")
+
+    var showDeleteDialog by remember { mutableStateOf(false) }
 
     Column(modifier = Modifier.fillMaxSize()) {
         Column(
@@ -70,9 +71,34 @@ fun SettingsScreen(settings: SettingsViewModel) {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Bottom
         ) {
-            Button(onClick = { settings.deleteAllFavorites() }) {
+            Button(onClick = { showDeleteDialog = true }) {
                 Text("Delete Favs")
             }
+        }
+
+        if (showDeleteDialog) {
+            AlertDialog(
+                onDismissRequest = {
+                    showDeleteDialog = false
+                }, // Se cierra si el usuario pulsa fuera
+                title = { Text(text = "Confirmar eliminación") },
+                text = { Text("¿Estás seguro de que quieres borrar todos los clanes favoritos? Esta acción no se puede deshacer.") },
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            settings.deleteAllFavorites() // Aquí es donde realmente borras
+                            showDeleteDialog = false      // Y cierras el diálogo
+                        }
+                    ) {
+                        Text("Eliminar todo")
+                    }
+                },
+                dismissButton = {
+                    Button(onClick = { showDeleteDialog = false }) {
+                        Text("Cancelar")
+                    }
+                }
+            )
         }
     }
 }
